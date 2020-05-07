1bbc6dbf7b6cc521815de6d7b87d5fe94cb547f5
Paul Albei <palbei.ext@synthesio.com>
2020-05-07 14:32:44 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import facts from "./facts";
+
+import "./App.scss";
 
 function App() {
+  const item = facts[Math.floor(Math.random() * facts.length)];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="title">New fact in every new tab</div>
+        <div className="fact">{item}</div>
+      </div>
     </div>
   );
 }
